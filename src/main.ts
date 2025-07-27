6a0e144f8afeea0563e6c083196cd656b6ec8137
akaryaka <akariaka@yandex.ru>
2025-07-28 02:09:28 +0600

    ref: main, counter, package.json

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
+import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    </a>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter();
