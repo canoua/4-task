209155185ecaa4d714c0097a6babac4185175b24
unknown <akariaka@yandex.ru>
2025-08-05 23:31:11 +0600

    add eventlisteners

--- src/components/input.ts
+++ src/components/input.ts
@@ -4,12 +4,30 @@ export function input() {
   const range = document.createElement("div");
   const wrapper = document.createElement("div");
 
-  range.setAttribute('draggable', 'true');
   wrapper.classList.add('wrapper');
   range.classList.add('range');
   line.classList.add('line');
   wrapper.appendChild(range);
   wrapper.appendChild(line);
+  app?.appendChild(wrapper);
 
-  app?.appendChild(wrapper)
+  let dragging = false;
+
+  range.addEventListener('mousedown', draggableFunc);
+  // range.addEventListener('mouseup', function() {
+  //   range.setAttribute('draggable', 'false')
+  // });
+
+  
+  function draggableFunc() {
+    range.setAttribute('draggable', 'true')
+  }
+
+  range.addEventListener('mouseup', function() {
+    console.log('mouseup');
+  })
+
+  document.addEventListener('mousemove', function() {
+    console.log('mousemove');
+  })
 }
