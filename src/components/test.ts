5c319900c1e61303840ed7068068eaa2dd96742d
unknown <akariaka@yandex.ru>
2025-10-11 01:42:04 +0600

    add mouseUp method

--- src/components/test.ts
+++ src/components/test.ts
@@ -2,55 +2,36 @@ export function test() {
   class Input {
     range: any = document.getElementById('range');
        // координаты
-    x: Number = 0;
-    y: Number = 0;
+    x: any = 0;
+    y: any = 0;
     id: any;
 
-    constructor(id: any, mouseDown?: any, range?: any) {
-      range = this.range;
-      id = this.id;
-      mouseDown = this.mouseDown;
-      mouseDown(range);
-    }
-
-    dragInput(x?: Number | any, y?: Number | any, range?: HTMLElement, listener?: any) {
-      range = this.range;
-      listener = this.listener;
-      x = this.x;
-      y = this.y;
-    
-      this.mouseDown(range);
-    }
-
-    mouseDown(elem?: any) {
-      elem.addEventListener('mousedown', function(e: any) {
-        console.log('mousedown');
-        document.addEventListener('mousemove', function() {
-          console.log('mousemove');
-        })
-      })
-    }
-
-    mouseDownFunc() {
+    constructor(range?: any) {
+      this.range = range; 
+      document.addEventListener('mousedown', this.mouseDown);
+      document.addEventListener('mouseup', this.mouseUp);
+      this.mouseMove;
       
     }
 
+    mouseDown() {
+      console.log('mouseDown');
+    }
 
-    listener(e: any, x: Number | any, y: Number | any, range: HTMLElement): void {
-      x = this.x;
-      y = this.y;
-      console.log('listener');
+    mouseMove(e: MouseEvent) {
+      console.log('mouseMove');
+      this.range.style.left = `${e.pageX - this.x}px`;
+      this.range.style.top = `${e.pageY - this.y}px`;
+      console.log(this.x);
+      
+    }
 
-
-      // ! - говорит о том, что уверен в типе данных?
-      range = this.range;
-
-      this.range.style.left = e.pageX - x + "px";
-      this.range.style.top = e.pageY - y + "px";
+    mouseUp() {
+      console.log('mouseUp');
     }
   }
 
   document.addEventListener('DOMContentLoaded', function() {
-    const input = new Input('input');
+    const input = new Input();
   })
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 // import { input } from './components/input'
 import { test } from './components/test'
 import './style.css'
+// import { newtest } from './components/newtest'
+
+// newtest()
 
 test()
 // input()
