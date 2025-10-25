1adefeb7a702ade93d2cb1734b5db495f2c33d06
unknown <akariaka@yandex.ru>
2025-10-26 02:15:09 +0600

    bug fixed

--- src/components/range.ts
+++ src/components/range.ts
@@ -1,29 +1,41 @@
 export function range() {
   class Input {
     range: any = document.createElement('div');
+    line: any = document.createElement('div');
+   
     // range_1: any = document.createElement('div');
 
     app: any;
-    line: HTMLElement | undefined;
-    wrapper: HTMLElement | undefined;
+    // line: any;
+    // wrapper: any;
     outputValue: any;
     // координаты мыши
     x: any = 0;
-    x_1: any = 0;
+    // x_1: any = 0;
 
+    wrapper:any = document.createElement('div');
     // определяем координаты ползунка
     rangeCoords: any = this.range.getBoundingClientRect();
+    wrapperCoords: any = this.wrapper.getBoundingClientRect();
+    lineCoords: any = this.line.getBoundingClientRect();
     // rangeCoords_1: any = this.range_1.getBoundingClientRect();
+
+    lineCoordsObj: any = {
+      x: this.lineCoords.left
+    }
+
+    wrapperCoordsObj: any = {
+      x: this.wrapperCoords.left
+    }
 
     // координаты ползунка
     coords: any = {
-        x: this.rangeCoords.left,
+      x: this.rangeCoords.left
         // x_1: this.rangeCoords_1.left,
-        // minX: 0,
-        // maxX: 80
     };
 
     positionRange: any;
+      
 
     // свойства обработчиков событий мыши
     handlerMouseDown: any;
@@ -36,17 +48,18 @@ export function range() {
       this.handlerMouseDown = this.mouseDown.bind(this);
       this.handlerMouseUp = this.mouseUp.bind(this);
       this.handlerMouseMove = this.mouseMove.bind(this);
-      this.positionRange = this.newPositionRange.bind(this);
+      // this.positionRange = this.newPositionRange.bind(this);
 
       document.addEventListener('mousedown', this.handlerMouseDown);
       document.addEventListener('mouseup', this.handlerMouseUp);
+
     }
 
     initElememts() {
       // инициализация элементов
       this.app = document.getElementById('app')
-      this.line = document.createElement('div');
-      this.wrapper = document.createElement('div');
+      
+      // this.wrapper = document.createElement('div');
       this.range = document.createElement('div');
       // this.range_1 = document.createElement('div');
       this.outputValue = document.createElement('div');
@@ -64,6 +77,8 @@ export function range() {
       this.wrapper.appendChild(this.outputValue);
 
       this.outputValue.textContent='0';
+
+      console.log(this.wrapperCoords);
     }
 
     mouseDown() {
@@ -71,25 +86,33 @@ export function range() {
     }
 
     mouseMove(event: any) {
-      this.x = event.clientX - this.rangeCoords;
-      this.newPositionRange();
+      this.x = event.clientX-150;
+      console.log(this.x);
+      
+      if(this.x>=0 && this.x<=85) {
+        this.range.style.left = `${this.x}px`;
+        this.outputValue.textContent = `${this.x}`;
+      }
+     
+      // this.newPositionRange();
     }
 
     mouseUp() {
       document.removeEventListener('mousemove', this.handlerMouseMove);
-      this.newPositionRange();
+      // this.newPositionRange();
+      if(this.x>=0 && this.x<=85) {
+        this.range.style.left = `${this.x}px`;
+        this.outputValue.textContent = `${this.x}`;
+      }
+      console.log(this.lineCoords);
     }
 
-    newPositionRange() {
+    // newPositionRange() {
       // нужен рефактор)
-      if(this.x>=150 && this.x<=235) {
-        this.coords.x = this.x;
-        this.range.style.left = `${this.x-150}px`;
-        this.outputValue.textContent = `${this.x-150}`;
-      } else {
-        this.coords.x = 0;
-      }
-    }
+      // this.coords.x = this.x;
+      // this.range.style.left = `${this.x-this.coords.x}px`;
+      // this.outputValue.textContent = `${this.x}`;
+    // }
   }
 
     class Panel {
