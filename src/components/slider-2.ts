4219a008a93ad0c41937f51184e77aec96282e03
unknown <akariaka@yandex.ru>
2025-11-16 23:52:19 +0600

    add output, limits

--- src/components/slider-2.ts
+++ src/components/slider-2.ts
@@ -4,11 +4,10 @@ export function slider() {
     thumbMax: any = document.createElement('div');
     line: any = document.createElement('div');
     app: any;
- 
-    // x: any = 0;
+    outputValueMin: any;
+    outputValueMax: any;
     minX: any;
     maxX: any;
-    middleThumb: any;
     wrapper:any = document.createElement('div');
     rangeCoords: any = this.thumb.getBoundingClientRect();
     wrapperCoords: any = this.wrapper.getBoundingClientRect();
@@ -33,26 +32,26 @@ export function slider() {
     // инициализация элементов
     initElememts() {
       this.app = document.getElementById('app')
-     
+      this.outputValueMin = document.createElement('div');
+      this.outputValueMax = document.createElement('div');
       this.thumb.classList.add('thumb', 'thumb-min');  
       this.thumbMax.classList.add('thumb', 'thumb-max');  
       this.line.classList.add('line');
       this.wrapper.classList.add('wrapper');
-   
-      
+      this.outputValueMin.classList.add('output-value');
+      this.outputValueMax.classList.add('output-value');
       this.app.appendChild(this.wrapper);
       this.line.appendChild(this.thumb);
       this.line.appendChild(this.thumbMax);
       this.wrapper.appendChild(this.line);
-   
+      this.wrapper.prepend(this.outputValueMin);
+      this.wrapper.prepend(this.outputValueMax);
+      this.outputValueMin.textContent='Первое значение: 0';
+      this.outputValueMax.textContent='Второе значение: 0';
       const lineWidth = this.line.offsetWidth;
       const linePosition = this.line.getBoundingClientRect();
       const lineMaxPosition = lineWidth + linePosition.left;
-      const middleThumb = this.thumb.offsetWidth / 2;
-
-      this.middleThumb = middleThumb;
       this.minX = linePosition.left;
-      
       this.maxX = lineMaxPosition;
     }
 
@@ -66,27 +65,25 @@ export function slider() {
 
     mouseMove(e: any) {
       if(this.draggingAcces) {
-        this.thumb.style.left = `${e.clientX-this.minX}px`;
-        console.log(Math.trunc((e.clientX-this.minX)/85*100));
+        if(e.clientX>=this.minX && e.clientX<=235) {
+          this.thumb.style.left = `${e.clientX-this.minX}px`;
+          this.outputValueMin.textContent=`Первое значение: ${Math.trunc((e.clientX-this.minX)/85*100)}`;
+        }
+        
       } else if(this.draggingAccesMax) {
-        this.thumbMax.style.left = `${e.clientX-this.minX}px`;
+        if(e.clientX>=this.minX && e.clientX<=235) {
+          this.thumbMax.style.left = `${e.clientX-this.minX}px`;
+          this.outputValueMax.textContent=`Второе значение: ${Math.trunc((e.clientX-this.minX)/85*100)}`;
+        }
       }
-        //  e.target.style.left = `${e.clientX-this.minX}px`;
-          
-        // }
-    
-    
     }
 
     mouseUp(e: any) {
-     if(e.target.classList.contains('thumb') && e.target.classList.contains('thumb-min')) {
-      this.draggingAcces = false;
-     } else if(e.target.classList.contains('thumb') && e.target.classList.contains('thumb-max')) {
-      this.draggingAccesMax = false;
-
-     }
-        
-     
+      if(e.target.classList.contains('thumb') && e.target.classList.contains('thumb-min')) {
+        this.draggingAcces = false;
+      } else if(e.target.classList.contains('thumb') && e.target.classList.contains('thumb-max')) {
+        this.draggingAccesMax = false;
+      }
     }
   }
 
