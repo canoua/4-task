a131c663708c631bdcacafc0bbbed57abb5253d0
unknown <akariaka@yandex.ru>
2026-02-01 00:18:26 +0600

    begin getValues in panel.ts

--- src/components/panel.ts
+++ src/components/panel.ts
@@ -2,26 +2,32 @@ import { Slider } from "./slider/index.";
 
 export class Panel{
   slider: Slider | any;
-  app: HTMLElement | null ;
+  app: HTMLElement | null;
   title: any;
   container: any;
   inputStep: any;
   inputWidth: any;
   inputMin: any;
+  button: HTMLElement | null;
 
   constructor() {
     this.app = document.getElementById('app');
-    console.log(this.slider.app);
     
     this.container = document.createElement('div');
     this.container.classList.add('panel-container');
     this.title = document.createElement('h1');
+    this.button = document.createElement('button');
+    this.button.textContent = 'ввод';
+
+    // let getMessage = this.addValues('hello')
+    this.button.addEventListener('click', () => this.getValues(this.container.children.input.value));
     this.title.textContent = 'Панель для управления ползунком';
     this.app.appendChild(this.container);
-    this.container.appendChild(this.title);
 
+    this.container.appendChild(this.title);
+    this.container.appendChild(this.button);
     this.createInput(this.container, 'input', 'input', 'step')
-    this.createInput(this.container, 'input', 'input', 'step')
+    // this.createInput(this.container, 'input', 'input', 'step')
   }
 
   createInput(parent?: any, element?: any, name?: string, placeholder?: string, className?: string) {
@@ -31,5 +37,11 @@ export class Panel{
     element.setAttribute('placeholder', placeholder);                                 
     element.classList.add(className);
     parent.appendChild(element);
+  }
+
+
+  getValues(element: any) {
+    console.log(element);
+    this.container.children.input.value = '';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { Panel } from './components/panel.ts';
 import './style.css'
 
 document.addEventListener('DOMContentLoaded', function() {
-  const slider = new Slider(50, 75, 10);
+  const slider = new Slider(50, 75, 1);
   const panel = new Panel();
 })
